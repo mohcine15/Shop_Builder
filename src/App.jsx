4b6d1c10c7d2689212d5bb1982afc6b0ea1bce9c
mohcine15 <mohcinesalhi7@gmail.com>
2024-07-29 11:35:51 +0200

    main modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-import React, { useState } from 'react';
-import './App.css';
-import Login from './components/Login_Register/Login.jsx';
-import Footer from './components/Footer/Footer.jsx';
-import NavBar from './components/Login_Register/NavBar_log_reg/NavBar.jsx';
-import Register from './components/Login_Register/Register.jsx';
-
-function App() {
-  const [currentComponent, setCurrentComponent] = useState('register');
-
-  const showLogin = () => {
-    setCurrentComponent('login');
-  };
-
-  const showRegister = () => {
-    setCurrentComponent('register');
-  };
-
-  return (
-    <div className="App">
-      <div className="main-content">
-      <NavBar showLogin={showLogin} showRegister={showRegister} />
-        {currentComponent === 'register' ? (
-          <Register showLogin={showLogin} />
-        ) : (
-          <Login showRegister={showRegister} />
-        )}
-        <Footer />
-      </div>
-=======
 import React from "react";
 import Nav from "./components/Navbar/navbar";
 import Head from "./components/Header/header"
@@ -50,13 +19,8 @@ function App(){
       <Ctc />
       <Foot />
  
->>>>>>> login_register
     </div>
   );
 }
 
-<<<<<<< HEAD
-export default App;
-=======
 export default App ;
->>>>>>> login_register
